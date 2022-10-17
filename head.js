166988bb4413bc20da6926f02fff36d94af27fe0
doublefriedtofu <hurdonghyuk@gmail.com>
2022-10-17 19:25:20 -0400

    Fixed the Issue

--- head.js
+++ head.js
@@ -5,7 +5,12 @@ const assertEqual = function(actual, expected) {
     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+// Function Implementation
+const head = function(myArray) {
+  return myArray[0];
+};
 
 // Test Code
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello");
