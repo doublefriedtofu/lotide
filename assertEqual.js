5286553fed96ebd8454cfa072091f5346cb36105
doublefriedtofu <hurdonghyuk@gmail.com>
2022-10-17 18:41:26 -0400

    assertEqual function to check if two are equal or not using emoji

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🔴🔴🔴' + 'Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('✅✅✅' + 'Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// Test Code
+assertEqual("vitamin D", "vitamin D");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(3, 5);
